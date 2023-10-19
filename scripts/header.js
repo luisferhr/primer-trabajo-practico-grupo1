999989d520460d8f63f6e6df4ba858590c7caf79
Luis Hernandez <hernandezluis.umc@gmail.com>
2023-10-18 23:10:01 -0300

    resolviendo conflictos

--- scripts/header.js
+++ scripts/header.js
@@ -2,13 +2,10 @@ const toggleBtn = document.querySelector(".toggle_btn")
 const toggleBtnIcon = document.querySelector(".toggle_btn i")
 const dropdownMenu = document.querySelector(".dropdown_menu")
 
-<<<<<<< HEAD:header.js
-=======
 $(document).ready(function(){
   $("#header").load("header.html");
 });
 
->>>>>>> 86d0b4acfa92e0f7331b2eb8a67fc151cdad820f:scripts/header.js
 toggleBtn.onclick = function(){
   dropdownMenu.classList.toggle('open')
   const isOpen = dropdownMenu.classList.contains('open')
@@ -19,10 +16,7 @@ toggleBtn.onclick = function(){
 }
 
 
-<<<<<<< HEAD:header.js
-=======
  
->>>>>>> 86d0b4acfa92e0f7331b2eb8a67fc151cdad820f:scripts/header.js
 
 
 
