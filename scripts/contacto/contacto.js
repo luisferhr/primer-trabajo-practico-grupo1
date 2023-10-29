6a770e5aa6a1821d71f289380c75c5c8d75afe08
Luis Hernandez <hernandezluis.umc@gmail.com>
2023-10-29 19:39:44 -0300

    Trabajo redireccionando a proyecto en formpree

--- scripts/contacto/contacto.js
+++ scripts/contacto/contacto.js
@@ -1,14 +1,12 @@
 miFormularioContacto = document.getElementById("miFormularioContacto")
 
 function validarNumeroTelefono(numero) {
-    // Define una expresión regular que coincide con un número de teléfono en formato (123) 456-7890
     var regex = /^\d{9}$/;
 
-    // Usamos el método test de la expresión regular para verificar si el número coincide
     if (regex.test(numero)) {
-        return true; // El número de teléfono es válido
+        return true; 
     } else {
-        return false; // El número de teléfono no es válido
+        return false; 
     }
 }
 
@@ -16,6 +14,66 @@ function esEmailValido(email) {
     const regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
     return regexEmail.test(email);
 }
+
+miFormularioContacto.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let nombre = document.getElementById("nombre"); 
+    let telefono = document.getElementById("telefono");
+    let email = document.getElementById("email");
+    let asunto = document.getElementById("asunto");
+    let descripcion = document.getElementById('descripcion');
+
+    if(nombre.value.length >= 3){
+        if(validarNumeroTelefono(telefono.value)){
+            if(esEmailValido(email.value)){
+                if(asunto.value.length >= 5){
+                    if(descripcion.value.length >= 5){
+                        console.log("Nombre: " + nombre.value);
+                        console.log("Telefono: " + telefono.value);
+                        console.log("Email: " + email.value);
+                        console.log("Asunto: " + asunto.value);
+                        console.log("Descripcion: " + descripcion.value)
+                    
+                        let formData = new FormData();
+                            formData.append("nombre", nombre.value);
+                            formData.append("telefono", telefono.value);
+                            formData.append("email", email.value);
+                            formData.append("asunto", asunto.value);
+                            formData.append("descripcion", descripcion.value);
+
+                        miFormularioContacto.reset();
+
+                        fetch(miFormularioContacto.action, {
+                            method: "POST",
+                            body: formData
+                        })
+                    }
+                    else{
+                        alert('Complete campo Descripcion. ')
+                        descripcion.focus()
+                    }
+                }
+                else{
+                    alert('Describa el asunto.')
+                    asunto.focus()
+                }
+            }
+            else {
+                alert('Describa un email valido')
+                email.focus()
+            }
+        }
+        else{
+            alert('Describa un numero telefonico correcto. (9 digitos) 912345678')
+        }
+    }
+    else{
+        alert('Ingrese su nombre')
+        nombre.focus()
+    }
+})
 
 
 
--- scripts/cotiza/cotiza.js
+++ scripts/cotiza/cotiza.js
@@ -1,14 +1,12 @@
 formulario = document.getElementById("miFormulario")
 
 function validarNumeroTelefono(numero) {
-    // Define una expresión regular que coincide con un número de teléfono en formato (123) 456-7890
     var regex = /^\d{9}$/;
 
-    // Usamos el método test de la expresión regular para verificar si el número coincide
     if (regex.test(numero)) {
-        return true; // El número de teléfono es válido
+        return true; 
     } else {
-        return false; // El número de teléfono no es válido
+        return false; 
     }
 }
 
@@ -17,4 +15,85 @@ function esEmailValido(email) {
     return regexEmail.test(email);
 }
 
+formulario.addEventListener("submit", function(event){
 
+    event.preventDefault();
+    console.log(event)
+
+    let nombre = document.getElementById("nombre");
+    let edad = document.getElementById("edad");
+    let telefono = document.getElementById('telefono');
+    let email = document.getElementById("email");
+    let zona = document.getElementById('zona');
+    let tamano = document.getElementById('tamano').value;
+    let descripcion = document.getElementById('descripcion');
+
+    let tipo = "";
+    if(document.getElementById("b-g").checked){
+        tipo = "Blanco y Negro"
+    }
+    else if(document.getElementById("detallesColor").checked){
+        tipo = "Detalles con Color"
+    }
+    else if(document.getElementById("color").checked){
+        tipo = "Full Color"
+    }
+
+    if (nombre.value.length >= 3) {
+        if (edad.value >= 18) {
+            if (validarNumeroTelefono(telefono.value)) {
+                if (esEmailValido(email.value)) {
+                    if (zona.value.length >= 5) {
+                        if (tipo.length > 1) {
+                            console.log("Nombre: " + nombre.value);
+                            console.log("Edad: " + edad.value);
+                            console.log("Telefono: " + telefono.value);
+                            console.log("Email: " + email.value);
+                            console.log("Zona: " + zona.value); 
+                            console.log("Tamaño: " + tamano + " cm");
+                            console.log("Descripcion: " + descripcion.value);
+                            console.log("Tipo de Tatuaje: " + tipo);
+                            
+                            let formData = new FormData();
+                            formData.append("nombre", nombre.value);
+                            formData.append("edad", edad.value);
+                            formData.append("telefono", telefono.value);
+                            formData.append("email", email.value);
+                            formData.append("zona", zona.value);
+                            formData.append("tamano", tamano);
+                            formData.append("descripcion", descripcion.value);
+                            formData.append("tipo", tipo);
+
+                            formulario.reset();
+
+                            fetch(formulario.action, {
+                                method: "POST",
+                                body: formData
+                            })
+                        } else {
+                            alert('Seleccione el tipo de color del tatuaje');
+                        }
+                    } else {
+                        alert('Describa una zona del cuerpo');
+                        zona.focus();
+                    }
+                } else {
+                    alert('Escriba un email válido');
+                    email.focus();
+                }
+            } else {
+                alert('Ingrese un teléfono correcto del formato 912345678 (9 dígitos)');
+                telefono.focus();
+            }
+        } else if (edad.value < 18 && edad.value > 0) {
+            alert('Debe ser mayor de edad para poder cotizar por un tatuaje');
+            edad.focus();
+        } else {
+            alert('Ingrese una edad válida');
+            edad.focus();
+        }
+    } else {
+        alert('Ingrese un nombre válido');
+        nombre.focus();
+    }
+});
